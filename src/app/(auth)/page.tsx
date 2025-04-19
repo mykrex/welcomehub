5fa74c16e99551fbefabb1b9db90121da648312d
MariaMartinez <marymmartynez@gmail.com>
2025-04-19 16:22:23 -0600

    update

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -101,7 +101,7 @@ export default function Home() {
                   </div>
                   <div className="flex justify-end">
                     <Link href="/olvide-contrasena" className="text-blue-500 hover:text-blue-600 underline">
-                    ¿Olvidaste tu contraseña o es tu primer acceso?
+                    ¿Olvidaste tu contraseña?
                     </Link>
                   </div>
                   <button type="submit" className="w-full bg-gradient-to-r from-blue-400 to-blue-600 text-white py-2 rounded-md hover:from-blue-600 hover:to-blue-700 transition">Ingresar</button>
