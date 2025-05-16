64b084cf2c9203adc9f2993642ab99d0adcf052c
A00834245 <89872076+A00834245@users.noreply.github.com>
2025-05-16 10:18:47 -0600

    Redirigir cursos a ver cursos dinamicamente

--- src/app/(authed)/cursos/verCurso/[id]/page.tsx
+++ src/app/(authed)/cursos/verCurso/[id]/page.tsx
@@ -1,7 +1,6 @@
 // Vercurso.tsx
 "use client";
 import React from "react";
-//*import { useSearchParams } from "next/navigation"; 
 import { mockCourses } from "@/app/api/cursos/verCurso/mockCourses";
 
 //* STYLES *//
@@ -14,37 +13,25 @@ import CourseDescription from "./assets/courseDesc";
 import StatsCard from "./assets/StatsCard";
 import Modules from "./assets/modules";
 
-export default function VerCurso() {
-  {
-    /* HOOKS 
-  const searchParams = useSearchParams();
-  const idParam = searchParams?.get("id");
- 
-  const id = idParam ? Number(idParam) : null;
-   */
-  }
-  const id = 1;
-  if (!id) return <div>ID de curso inválido.</div>;
-
+export default function VerCurso({ params }: { params: { id: string } }) {
+  const id = Number(params.id);
   const course = mockCourses.find((c) => c.id === id);
-  if (!course) return <div>Curso no encontrado.</div>;
+
+  if (!course) return <div>Curso no encontrado</div>;
+
   return (
     <div className="main-content">
       <div className="main-inner">
-        {/* MI PROGRESO */}
         <ProgressBar />
 
-        {/* ROW 1 */}
         <div className="course-row">
           <CourseDescription course={course} />
-
           <div className="course-stats">
             <StatsCard />
             <StatsCard />
           </div>
         </div>
 
-        {/* Modulos*/}
         <Modules course={course} />
       </div>
     </div>
